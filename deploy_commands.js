cfe584ad41479432438a8ece3a8b7c63084a6192
RodolfoFigueroa <rodfiso@ciencias.unam.mx>
2021-10-06 17:44:05 -0500

    Made commands global

--- deploy_commands.js
+++ deploy_commands.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -10,12 +10,19 @@ for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
+
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
-
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID_2), { body: commands })
-    .then(() => console.log('Successfully registered application commands 2.'))
-    .catch(console.error);
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands },
+        );
+        console.log('Successfully reloaded application (/) commands.');
+    }
+    catch (error) {
+        console.error(error);
+    }
+})();
